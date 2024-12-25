87844e066580d00389a536adca42bceb66a426e6
Зеленова Мария Алексеевна <osen13@yandex.ru>
2024-12-25 15:44:48 +0300

    Хищники и машины

---
+++ module_6_1.py
@@ -0,0 +1,66 @@
+# Задача "Съедобное, несъедобное":
+
+class Animal:
+    def __init__(self, name, alive=True, fed=False):
+        self.name = name
+        self.alive = alive
+        self.fed = fed
+
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+
+        else:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+class Plant:
+    def __init__(self, name, edible=False):
+        self.name = name
+        self.edible = edible
+
+
+class Mammal(Animal):
+    def eat(self, food):
+        super().eat(food)
+class Predator(Animal):
+    def eat(self, food):
+        if food.edible:
+            print(f'{self.name} не стал есть {food.name}')
+            self.alive = False
+
+        else:
+            print(f'{self.name} съел {food.name}')
+            self.fed = True
+
+
+class Flower(Plant):
+    def __init__(self, name, edible=True):
+        super().__init__(name, edible)
+
+class Fruit(Plant):
+    def __init__(self, name, edible=True):
+        super().__init__(name, edible)
+
+# Выполняемый код(для проверки):
+
+a1 = Predator('Волк с Уолл-Стрит')
+
+a2 = Mammal('Хатико')
+
+p1 = Flower('Цветик семицветик')
+
+p2 = Fruit('Заводной апельсин')
+
+print(a1.name)
+print(p1.name)
+print(a1.alive)
+print(a2.fed)
+
+a1.eat(p1)
+a2.eat(p2)
+
+print(a1.alive)
+print(a2.fed)
+
